9b265965c7c5220d783f0305142eec30696269c1
Martin Manomat <manomatmartin@gmail.com>
2017-11-08 13:28:23 -0500

    New edit with side nav bar

---
+++ public_html/assets/js/navBar.js
@@ -0,0 +1,22 @@
+var clicked = 0;
+
+function nav() {
+
+    if (clicked == 0) {
+   
+            document.getElementById("mySidenav").style.width = "15em";
+            document.getElementById("main").style.marginLeft = "16em";
+            clicked = 1;
+        
+    }
+
+    else if (clicked == 1) {
+        
+            document.getElementById("mySidenav").style.width = "0";
+            document.getElementById("main").style.marginLeft = "0";
+            clicked = 0;
+
+    }
+
+
+}
