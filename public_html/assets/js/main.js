28723395cb1c7e865be978c7480f5b5481c175f7
Marty <manomatmartin@gmail.com>
2017-11-14 17:25:09 -0500

    Changed navbar and footer. Added dynamic scrolly

--- public_html/assets/js/main.js
+++ public_html/assets/js/main.js
@@ -4,6 +4,10 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+function changeText() {
+    document.getElementById("ScrollButton").innerHTML = "Enjoy!"
+}
+
 (function($) {
 
 	skel.breakpoints({
